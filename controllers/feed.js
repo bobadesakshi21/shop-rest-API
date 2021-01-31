ed2a5b20809db674c602c2073eaf69fe76b3f81b
Sakshi Bobade <bobadesakshi21@gmail.com>
2021-01-31 13:35:44 +0530

    Edit And Delete Post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-unused-vars */
+const fs = require('fs')
+const path = require('path')
 
 const { validationResult } = require('express-validator')
 
@@ -83,5 +85,94 @@ exports.getPost = (req, res, next) => {
       }
       next(err)
     })
+}
 
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId
+
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation Failed')
+    error.statusCode = 422
+    throw error
+  }
+
+  const title = req.body.title
+  const content = req.body.content
+
+  let imageUrl = req.body.image
+  if (req.file) {
+    imageUrl = req.file.path
+  }
+
+  if (!imageUrl) {
+    const err = new Error('No file picked')
+    err.statusCode = 422
+    throw err
+  }
+
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find post')
+        error.statusCode = 404
+        throw error
+      }
+      post.title = title
+      post.imageUrl = imageUrl
+      post.content = content
+
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl)
+      }
+
+      return post.save()
+    })
+    .then(result => {
+      res.status(200).json({
+        message: 'Post updated successfully!',
+        post: result
+      })
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500
+      }
+      next(err)
+    })
+}
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find post')
+        error.statusCode = 404
+        throw error
+      }
+      //check the logged in user 
+
+      clearImage(post.imageUrl)
+
+      return Post.findByIdAndRemove(postId)
+    })
+    .then(result => {
+      console.log('DELETE RESULT: ', result)
+      res.status(200).json({
+        message: 'Post deleted successfully!'
+      })
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500
+      }
+      next(err)
+    })
+}
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath)
+  fs.unlink(filePath, err => console.log(err))
 }
