4814da3781444f5ab497828979365cd3b40b4ebd
Sakshi Bobade <bobadesakshi21@gmail.com>
2021-01-31 10:20:29 +0530

    Adding the post Model and creating a POst collection

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
 const feedRoutes = require('./routes/feed')
+
+const app = express()
+
+const MONGODB_URI = 'mongodb+srv://Sakshi:sakshi123@cluster0.vpzlm.mongodb.net/messages?retryWrites=true&w=majority'
 
 // app.use(bodyParser.urlencoded({})) // x-www-form-urlencoded
 app.use(bodyParser.json()) //application/json
@@ -13,5 +18,13 @@ app.use((req, res, next) => {
   next()
 })
 app.use('/feed', feedRoutes)
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    app.listen(3000)
+  })
+  .catch(err => {
+    console.log('CONNECTION ERR', err)
+  })
 
 app.listen(8080)
