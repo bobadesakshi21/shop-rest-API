54a5aa87aca49fae5853ba1392104239ca944e1e
Sakshi Bobade <bobadesakshi21@gmail.com>
2021-02-04 22:47:40 +0530

    Socket.js added

--- app.js
+++ app.js
@@ -66,12 +66,7 @@ app.use((error, req, res, next) => {
 mongoose.connect(MONGODB_URI)
   .then(() => {
     const server = app.listen(8080)
-    const io = require('socket.io')(server, {
-      cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PATCH', 'DELETE']
-      }
-    })
+    const io = require('./socket').init(server)
     io.on('connection', socket => {
       console.log('Connection Established')
     })
